8183eee16e8e34640346fb029e6382dc1d179952
vijethmd <vijethmd.cs23@bmsce.ac.in>
2025-11-17 18:13:14 +0530

    Update SimulatedAnnealing.py

--- Lab5/SimulatedAnnealing.py
+++ Lab5/SimulatedAnnealing.py
@@ -1,44 +1,80 @@
-import math
 import random
+import math
 
-def simulated_annealing(initial_state, cost_function, neighbour_function,
-                        T_start=1000, T_end=1e-6, alpha=0.99):
 
+def compute_cost(state):
+    cost = 0
+    n = len(state)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            # Same row
+            if state[i] == state[j]:
+                cost += 1
+
+            # Same diagonal
+            if abs(state[i] - state[j]) == abs(i - j):
+                cost += 1
+
+    return cost
+
+
+
+def get_random_neighbor(state):
+    n = len(state)
+    new_state = state[:]
+
+    col = random.randint(0, n - 1)          
+    row = random.randint(1, n)              
+
+    new_state[col] = row
+    return new_state
+
+
+def simulated_annealing(initial_state):
     current = initial_state
-    T = T_start
+    current_cost = compute_cost(current)
 
-    while T > T_end:
-        next_state = neighbour_function(current)
-        current_cost = cost_function(current)
-        next_cost = cost_function(next_state)
-        dE = current_cost - next_cost
+    T = 30               
+    Tmin = 0.1           
+    alpha = 0.95         
 
-        if dE > 0:
+    while T > Tmin and current_cost != 0:
+
+       
+        next_state = get_random_neighbor(current)
+        next_cost = compute_cost(next_state)
+
+        deltaE = next_cost - current_cost
+
+        
+        if deltaE < 0:
             current = next_state
+            current_cost = next_cost
         else:
-            p = math.exp(dE / T)
-            if random.random() < p:
+            prob = math.exp(-deltaE / T)
+            if random.random() < prob:
                 current = next_state
+                current_cost = next_cost
 
+        
         T *= alpha
 
-    return current
+    return current, current_cost
 
 
-def cost(x):
-    return x * x
 
-def neighbour(x):
-    return x + random.uniform(-1, 1)
+if __name__ == "__main__":
+   
+    initial_state = [random.randint(1, 8) for _ in range(8)]
 
-best = simulated_annealing(
-    initial_state=random.uniform(-10, 10),
-    cost_function=cost,
-    neighbour_function=neighbour,
-    T_start=1000,
-    T_end=1e-6,
-    alpha=0.995
-)
+    print("Initial State:", initial_state)
+    final_state, final_cost = simulated_annealing(initial_state)
 
-print("Best solution found:", best)
-print("Cost:", cost(best))
+    print("\nFinal State:", final_state)
+    print("Final Heuristic (0 means success):", final_cost)
+
+    if final_cost == 0:
+        print("\nSolution FOUND!")
+    else:
+        print("\nStopped at local minimum / annealing ended.")
